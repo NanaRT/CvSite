2b68b96df0f800445232b3da867182da5334b73f
NanaRT <alanataylor@hotmail.fr>
2015-09-20 15:04:02 +0200

    Third Commit

--- src/CvsiteBundle/Controller/DefaultController.php
+++ src/CvsiteBundle/Controller/DefaultController.php
@@ -3,11 +3,40 @@
 namespace CvsiteBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
 
 class DefaultController extends Controller
 {
-    public function indexAction()
+    public function accueilAction()
     {
-        return $this->render('CvsiteBundle:Default:index.html.twig');
+        return $this->render('CvsiteBundle:Default:accueil.html.twig');
+    }
+	
+	public function telechargementCvPdfAction()
+	{
+		$fichier = "CV Alana TAYLOR french.pdf";
+	    $chemin = "bundles/cvsite/documents/"; // emplacement de votre fichier .pdf
+	         
+	    $response = new Response();
+	    $response->setContent(file_get_contents($chemin.$fichier));
+	    $response->headers->set('Content-Type', 'application/force-download'); // modification du content-type pour forcer le téléchargement (sinon le navigateur internet essaie d'afficher le document)
+	    $response->headers->set('Content-disposition', 'filename='. $fichier);
+	         
+	    return $response;
+	}
+	
+    public function formationAction()
+    {
+        return $this->render('CvsiteBundle:Default:formation.html.twig');
+    }
+	
+    public function professionelAction()
+    {
+        return $this->render('CvsiteBundle:Default:professionel.html.twig');
+    }
+	
+    public function projetsAction()
+    {
+        return $this->render('CvsiteBundle:Default:projets.html.twig');
     }
 }
